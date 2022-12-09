8fcb563f9c2bf52baa5f9f1c0fabbdfec6db9e20
Aidashko <aidana20051318@gmail.com>
2022-12-09 20:24:50 +0600

    hometask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+
+// task 1
+// Создать компонент в котором будет блок, инпут и кнопка, в инпут необходимо вводить цвет фона блока и по нажатию на кнопку менять цвет фона при этом очищая инпут
 
 function App() {
+  const [color1, setColor1] = useState("");
+
+  const clickOn = (color1) => {
+    setColor1();
+  };
+  useEffect(() => {
+    setColor1(color1);
+  }, [color1]);
+
+  //Task2
+  const [res, setRes] = useState("");
+
+  console.log(res);
+
+  function counter(res) {
+    // let result = +0;
+    if (res <= 18) {
+      alert("Welcome!");
+    }
+    if (res >= 18) {
+      alert("You should be 18!");
+    }
+    setRes(res);
+  }
+  useEffect(() => {
+    setRes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" align="center">
+      <input
+        type="text"
+        value={color1}
+        onChange={(e) => setColor1(e.target.value)}
+      />
+      <button value={color1} onClick={clickOn}>
+        Change Color
+      </button>
+      <div style={{ backgroundColor: color1 }}>{color1}</div>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your age"
+          value={res}
+          onChange={(e) => setRes(e.target.value)}
+        />
+        <button onClick={counter}>Submit</button>
+      </div>
     </div>
   );
 }
